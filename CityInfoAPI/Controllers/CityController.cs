1671b47b5062599ad49ad08faf1d0b4194d5e913
Jorge Miguel Hernandez Santos <dev.jhernandez@gmail.com>
2018-03-30 23:39:45 -0400

    proyect created

---
+++ CityInfoAPI/Controllers/CityController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CityInfoAPI.Controllers
+{   [Route("api/cities")]
+    public class CityController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [HttpGet()]
+        public JsonResult GetCities() {
+
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id) {
+
+            var citytoReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+
+            if (citytoReturn == null)
+            {
+                return NotFound();
+            }
+            else {
+                return Ok(citytoReturn);
+            }
+                
+        }
+
+
+    }
+}
---
+++ CityInfoAPI/Models/CityDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfoAPI.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int NumberOfPointsInterest { get; set; }
+
+        public ICollection<PointsOfInterestDto> PointsOfInterest { get; set; }
+        = new List<PointsOfInterestDto>();
+    }
+}
